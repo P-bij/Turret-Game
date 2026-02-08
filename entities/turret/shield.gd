9f0bcc15d8f046fcf07e0542877ef44a6ddded6a
P-bij <pet.bij@gmail.com>
2026-02-08 07:23:05 -0600

    Cleaned up the shield script.

--- entities/turret/shield.gd
+++ entities/turret/shield.gd
@@ -3,44 +3,60 @@ extends Node2D
 
 @onready var flash_timer: Timer = $Timer
 
-@export var flash_time_seconds: float = .3
-@export var damage_intake: float = 100
+@export var flash_time: float = .3
 @export var damage_colour: Color = Color(1.0, 0.0, 0.0, 0.5)
 @export var normal_colour: Color = Color(.038, 1, 1, .196)
+@export var amount_of_damage_it_can_take: float = 100.0
 
-var scale_start: float = .1
-var target_scale_size: Vector2 = Vector2(1,1)
+const SCALE_DIVISOR: float = 100.0
+
+var scale_start: Vector2 = Vector2(.01,.01)
 var tween: Tween 
 var time_to_scale: float = .25
-var death_size: Vector2 = Vector2(.15,.15)
-
+var death_size: float = 15.0 	# When the shield is smaller than the turret 
+								# and can't be seen any longer.
+var health: float 
 
 func _ready() -> void:
-	if tween:
-		tween.kill()
+	health = amount_of_damage_it_can_take + death_size 
+	create_shield()
+
+
+func create_shield():
+	#shield grows from nothing to full size
+	kill_tween()
 	tween = get_tree().create_tween()
 	set_modulate(normal_colour)
-	set_scale(Vector2(scale_start,scale_start))
-	tween.tween_property(self,"scale",Vector2(1,1),time_to_scale)
+	set_scale(scale_start)
+	tween.tween_property(
+		self,"scale",Vector2(health,health) / SCALE_DIVISOR,time_to_scale)
 
 
 func _on_hitbox_component_area_entered(area: Area2D) -> void:
-	if area is HitboxComponent and damage_intake > 0:
+	if area is HitboxComponent and health > 0:
+		# flash from red to blue
 		set_modulate(damage_colour)
-		flash_timer.start(flash_time_seconds)
-		if damage_intake > area.damage:
-			if tween:
-				tween.kill()
+		flash_timer.start(flash_time)
+		health -= area.damage
+		if health > death_size:
+			kill_tween()
 			tween = get_tree().create_tween()
-			var current_scale_size: Vector2 = Vector2(target_scale_size)
-			damage_intake -= area.damage
-			var scale_subtraction = abs(area.damage/100)
-			target_scale_size = current_scale_size - Vector2(scale_subtraction, scale_subtraction)
-			tween.tween_property(self,"scale",Vector2(target_scale_size),time_to_scale)
-			await tween.finished
-			if target_scale_size <= death_size:
-				GSignals.shield_gone.emit()
-				queue_free()
+			# shrink shield by damage
+			tween.tween_property(self,"scale",Vector2(
+				health, health)/SCALE_DIVISOR,time_to_scale)
+		elif health <= death_size:
+			shield_death()
+
+
+func shield_death() -> void:
+	flash_timer.stop()
+	GSignals.shield_gone.emit()
+	queue_free()
+
+
+func kill_tween() -> void:
+	if tween:
+		tween.kill()
 
 
 func _on_timer_timeout() -> void:
