4c2373491299fb516a7a1e827f8f9a08a13bf793
P-bij <pet.bij@gmail.com>
2026-02-21 12:46:31 -0600

    Had to move the UI down the priorities list, so call_deferred was needed. Also, changed the screen size to be the visible_rect of the viewport.

--- UI/game_over_screen.gd
+++ UI/game_over_screen.gd
@@ -3,8 +3,8 @@ extends Control
 
 func _ready() -> void:
 	GSignals.begin_game_over.connect(game_over)
-	var screen_size: Vector2 = get_viewport().size
-	size = screen_size
+	var screen_size: Vector2 = get_viewport().get_visible_rect().size
+	set_deferred("size", screen_size)
 
 
 func game_over() -> void:
