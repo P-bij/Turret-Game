b87bfd125d310ada534e4e670e58c2a4e26b47df
P-bij <pet.bij@gmail.com>
2026-03-01 07:32:30 -0600

    added a reset_component that will delete the parent node when the reset is signal called.

---
+++ components/reset_component.gd
@@ -0,0 +1,13 @@
+@icon("res://assets/icons/reset-icon.png")
+extends Node2D
+
+@onready var papa: Node = get_parent()
+
+
+func _ready() -> void:
+	GSignals.reset.connect(start_over_again)
+
+
+func start_over_again() -> void:
+	if papa:
+		papa.queue_free()
