d7b2f54bc1044b78d5933ad504cc67acb42cccb1
P-bij <pet.bij@gmail.com>
2026-03-01 07:21:40 -0600

    Added a game_over variable that is false at the start and if the start_again button is pressed. And true if the game ends (turret dies). This prevents the menu key from closing the game over screen on a game over. created a toggle_menu function, which pauses/unpauses the game and makes the screen visible/hidden. Made the screen appear when the menu button is displayed. Changed it so that the Label text changes depending on whether game_over is true or not. Centered the Label text Added a start_again_button function that will call the reset signal, set game over to false, close the menu, change the menu label, and finally call the begin_the_game signal. Added the quit_button_pressed function to quit the game completely on press.

--- UI/game_over_screen.gd
+++ UI/game_over_screen.gd
@@ -1,11 +1,55 @@
 extends Control
 
+@onready var label: Label = %Label
+
+# if true, the game_over screen can't be closed by the menu button
+var game_over: bool = false 
+var label_text: String = "Menu"
+
 
 func _ready() -> void:
-	GSignals.begin_game_over.connect(game_over)
+	game_over = false
+	change_label()
+	GSignals.begin_game_over.connect(game_end)
 	var screen_size: Vector2 = get_viewport().get_visible_rect().size
 	set_deferred("size", screen_size)
 
 
-func game_over() -> void:
-	visible = true
+func change_label() -> void:
+	if game_over:
+		label_text = "GAME OVER"
+		label.label_settings.font_color = Color.RED
+	else:
+		label_text = "Menu"
+		label.label_settings.font_color = Color.ANTIQUE_WHITE
+	label.text = label_text
+
+
+func _shortcut_input(event: InputEvent) -> void:
+	if event.is_action_pressed("menu") and !game_over:
+		toggle_menu()
+
+
+func game_end() -> void:
+	game_over = true
+	change_label()
+	toggle_menu()
+
+
+func toggle_menu() -> void:
+	get_tree().paused = not get_tree().paused
+	visible = !visible
+
+
+func _on_start_again_button_pressed() -> void:
+	GSignals.reset.emit()
+	game_over = false
+	toggle_menu()
+	change_label()
+	GSignals.begin_the_game.emit()
+	print("begin the game")
+
+
+func _on_quit_button_pressed() -> void:
+	get_tree().root.propagate_notification(NOTIFICATION_WM_CLOSE_REQUEST)
+	get_tree().quit()
