87801c8943fe4a9452ad0b5d69b2d9b477ed55b5
P-bij <pet.bij@gmail.com>
2026-03-01 07:29:37 -0600

    Changed the bars_update function so that it calculates how many bars to add to the current count, and subtract bars if necessary.

--- UI/health_bar_container.gd
+++ UI/health_bar_container.gd
@@ -5,6 +5,7 @@ extends HBoxContainer
 const CONTAINER_CAPACITY: int = 100
 
 var health: float = 0.0
+var current_bars: int = 0
 var amount_of_bars: int = 1
 
 
@@ -36,7 +37,27 @@ func update_health_bar(new_value: float, body: String) -> void:
 
 func bars_update(value: float, body_name: String) -> void:
 	if body_name == "Turret":
-		amount_of_bars = ceil(value/CONTAINER_CAPACITY)
-		for i in amount_of_bars:
-			var healthbar = healthbar_scene.instantiate()
-			add_child(healthbar)
+		var new_bar_total = ceil(value/CONTAINER_CAPACITY)
+		var bars_to_add: int = new_bar_total - current_bars
+		if bars_to_add > current_bars:
+			for i: int in bars_to_add:
+				create_new_healthbar()
+		if bars_to_add == current_bars:
+			print("No change")
+			pass
+		if bars_to_add < current_bars:
+			for i: int in abs(bars_to_add):
+				delete_healthbar()
+		current_bars = new_bar_total
+
+
+func delete_healthbar() -> void:
+	var healthbars: Array[Node] = get_children()
+	for i: int in healthbars.size():
+		if healthbars[i] is ProgressBar:
+			healthbars[i].queue_free()
+
+
+func create_new_healthbar() -> void:
+	var healthbar = healthbar_scene.instantiate()
+	add_child(healthbar)
