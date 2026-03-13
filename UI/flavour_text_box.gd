6e1909d8421428618b2e38caea6198b92c56eac4
P-bij <pet.bij@gmail.com>
2026-03-13 00:32:07 -0600

    Created flavour text box that pops up when you hover over an upgrade or health.

---
+++ UI/flavour_text_box.gd
@@ -0,0 +1,27 @@
+extends Label
+
+@onready var timer: Timer = $Timer
+
+@export var time_out_seconds: float = 3.0
+
+
+func _ready() -> void:
+	GSignals.flavour_text_show.connect(display_flavour_text)
+	GSignals.flavour_text_hide.connect(hide_flavour_text)
+
+
+func display_flavour_text(description: String) -> void:
+	text = description
+	global_position = get_global_mouse_position()
+	global_position.x = clamp(global_position.x, 0, 1088)
+	global_position.y = clamp(global_position.y, 0, 376)
+	visible = true
+	timer.start(time_out_seconds)
+
+
+func hide_flavour_text() -> void:
+	visible = false
+
+
+func _on_timer_timeout() -> void:
+	hide_flavour_text()
