45d2539a30a06279f47fd41c26190b7f7e564629
P-bij <pet.bij@gmail.com>
2026-03-13 01:36:34 -0600

    Created powerup resources.

---
+++ systems/powerups/powerups.gd
@@ -0,0 +1,7 @@
+class_name PowerUp
+extends Resource
+
+
+@export var pu_type: Globals.PowerupType
+@export var pu_icon: Texture2D = preload("res://assets/images/none.png")
+@export var pu_description: String
